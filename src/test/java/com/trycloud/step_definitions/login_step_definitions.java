0386f77178dab15e16a7ac1580bbc4ec60f57645
Nesibe64 <sabancinesibe@gmail.com>
2022-03-25 15:20:10 -0500

    Initial commit

---
+++ src/test/java/com/trycloud/step_definitions/login_step_definitions.java
@@ -0,0 +1,33 @@
+package com.trycloud.step_definitions;
+
+import com.trycloud.utilities.ConfigurationReader;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class login_step_definitions {
+
+
+    @Given("user on the login page http:\\/\\/qa3.trycloud.net\\/index.php\\/login?clear={int}")
+    public void user_on_the_login_page_http_qa3_trycloud_net_index_php_login_clear(Integer int1) {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+    }
+    @When("user use username {string} and passcode “Userpass123\"")
+    public void user_use_username_and_passcode_userpass123(String string) {
+
+    }
+    @When("user click the login button")
+    public void user_click_the_login_button() {
+
+    }
+    @Then("verify the user should be at the dashboard page")
+    public void verify_the_user_should_be_at_the_dashboard_page() {
+
+    }
+
+
+
+
+
+}
